1d5cb272bdc3f7bef7126409948a120ed5a46823
Jeremy Rose <nornagon@nornagon.net>
2021-03-10 16:54:59 -0800

    refactor tests a bit

--- src/all.test.ts
+++ src/all.test.ts
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/svelte'
 import * as fs from 'fs'
 
-import { CddaData } from './data'
+import { CddaData, mapType } from './data'
 
 import Thing from './Thing.svelte';
 
@@ -21,15 +21,12 @@ const types = [
   'tool_quality',
   'skill',
 ]
-for (const ty of types) {
-  for (const item of data.byType(ty)) {
-    if (item.id)
-      test(`render ${ty} ${item.id}`, () => {
-        const { container } = render(Thing, { item: { type: ty, id: item.id}, data })
-        if (ty !== 'technique') {
-          expect(container.textContent).not.toMatch(/undefined/)
-          expect(container.textContent).not.toMatch(/NaN/)
-        }
-      })
+
+const all = data._raw.filter(x => x.id && types.includes(mapType(x.type))).map(x => [mapType(x.type), x.id])
+
+test.each(all)("render %s %s", (type, id) => {
+  const { container } = render(Thing, { item: { type, id}, data })
+  if (type !== 'technique') {
+    expect(container.textContent).not.toMatch(/undefined|NaN|object Object/)
   }
-}
+})
