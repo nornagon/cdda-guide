819c73b81949b3d1ab98d25bf3e200c37dc5dbca
Andrey Bienkowski <hexagon-recursion@posteo.net>
2021-09-01 12:57:43 +0300

    Begin auto-formatting

--- src/types/item/SpawnedIn.test.ts
+++ src/types/item/SpawnedIn.test.ts
@@ -1,7 +1,7 @@
-import { render } from '@testing-library/svelte'
-import SpawnedIn from './SpawnedIn.svelte'
-import WithData from '../../WithData.svelte'
+import { render } from "@testing-library/svelte";
+import SpawnedIn from "./SpawnedIn.svelte";
+import WithData from "../../WithData.svelte";
 
-test('TODO', () => {
-  render(WithData, {Component: SpawnedIn, item_id: "fake_item"})
-})
+test("TODO", () => {
+  render(WithData, { Component: SpawnedIn, item_id: "fake_item" });
+});
