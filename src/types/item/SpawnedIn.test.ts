168191288a2050dd4f2a088358d9dcdd1b2f7ac9
Andrey Bienkowski <hexagon-recursion@posteo.net>
2021-09-01 11:49:50 +0300

    Allow SpawnedIn.svelte to be tested in isolation
    Based on:
    https://svelte-recipes.netlify.app/testing/#testing-the-context-api

---
+++ src/types/item/SpawnedIn.test.ts
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/svelte'
+
+import IsolatedSpawnedIn from './IsolatedSpawnedIn.svelte'
+
+test('TODO', () => {
+  render(IsolatedSpawnedIn, {item_id: "fake_item"})
+})
