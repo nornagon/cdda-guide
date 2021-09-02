2bcc8a05f7c711189a534561c7b6493c42111513
Andrey Bienkowski <hexagon-recursion@posteo.net>
2021-09-02 08:45:02 +0300

    Implement getItemSpawnLocations()

---
+++ src/types/item/spawnLocations.test.ts
@@ -0,0 +1,23 @@
+import * as spawnLocations from "./spawnLocations";
+import { getItemSpawnLocations } from "./spawnLocations";
+import type { CddaData } from "../../data";
+
+beforeEach(() => jest.restoreAllMocks());
+
+describe("getItemSpawnLocations()", () => {
+  it("finds a one spawn location", () => {
+    let loot = [
+      {
+        omt: { singularName: "fake loc" },
+        loot: new Map([["fake_item", 0.25]]),
+      },
+    ];
+    jest.spyOn(spawnLocations, "getAllLocationsAndLoot").mockReturnValue(loot);
+
+    const locations = getItemSpawnLocations(null as CddaData, "fake_item");
+
+    expect(locations).toStrictEqual([
+      { singularName: "fake loc", chance: 0.25 },
+    ]);
+  });
+});
--- src/types/item/spawnLocations.ts
+++ src/types/item/spawnLocations.ts
@@ -1,8 +1,31 @@
-import type {CddaData} from '../../data'
+import type { CddaData } from "../../data";
+
+type OMT = {
+  // id: string[]
+  // sym: string
+  // color: string
+  singularName: string;
+};
+
+type LocationAndLoot = {
+  omt: OMT;
+  loot: Map</*item_id*/ string, /*chance*/ number>;
+};
+export function getAllLocationsAndLoot(data: CddaData): LocationAndLoot[] {
+  throw "not implemented";
+}
+
 type SpawnLocation = {
   singularName: string;
   chance: number;
 };
-export function getItemSpawnLocations(data: CddaData, item_id: string): SpawnLocation[] {
-  throw "not implemented";
+export function getItemSpawnLocations(
+  data: CddaData,
+  item_id: string
+): SpawnLocation[] {
+  // This is to allow tests to mock getAllLocationsAndLoot()
+  const x = exports.getAllLocationsAndLoot(data);
+  return x.flatMap(({ omt: { singularName }, loot }) =>
+    loot.has(item_id) ? [{ singularName, chance: loot.get(item_id) }] : []
+  );
 }
