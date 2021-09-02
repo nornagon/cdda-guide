0acb7a97eb405544f771348898124a74f8458f11
Andrey Bienkowski <hexagon-recursion@posteo.net>
2021-09-02 12:59:20 +0300

    collection() › returns nothing given no items

--- src/types/item/spawnLocations.test.ts
+++ src/types/item/spawnLocations.test.ts
@@ -1,5 +1,5 @@
 import * as spawnLocations from "./spawnLocations";
-import { getItemSpawnLocations } from "./spawnLocations";
+import { getItemSpawnLocations, collection } from "./spawnLocations";
 import type { CddaData } from "../../data";
 
 beforeEach(() => jest.restoreAllMocks());
@@ -8,7 +8,12 @@ describe("getItemSpawnLocations()", () => {
   it("finds a one spawn location", () => {
     let loot = [
       {
-        mapgen: { overmap_terrains: [{ singularName: "fake loc" }] },
+        mapgen: {
+          overmap_terrains: [{ singularName: "fake loc" }],
+          rows: [],
+          palettes: [],
+          additional_items: new Map(),
+        },
         loot: new Map([["fake_item", 0.25]]),
       },
     ];
@@ -19,5 +24,13 @@ describe("getItemSpawnLocations()", () => {
     expect(locations).toStrictEqual([
       { singularName: "fake loc", chance: 0.25 },
     ]);
+  });
+});
+
+describe("collection()", () => {
+  it("returns nothing given no items", () => {
+    const got = collection([]);
+
+    expect(got).toStrictEqual(new Map());
   });
 });
--- src/types/item/spawnLocations.ts
+++ src/types/item/spawnLocations.ts
@@ -1,5 +1,22 @@
 import type { CddaData } from "../../data";
 
+/** 0.0 < chance < 1.0 */
+type chance = number;
+
+type Loot = Map</**item_id*/ string, chance>;
+/** Independently choose whether to place each item  */
+export function collection(
+  items: Iterable<{ loot: Loot; chance: chance }>
+): Loot {
+  return new Map();
+}
+/** Choose based on weight */
+export function distribution(
+  items: Iterable<{ loot: Loot; weight: number }>
+): Loot {
+  throw "not implemented";
+}
+
 type OMT = {
   singularName: string;
   // sym: string
@@ -7,10 +24,28 @@ type OMT = {
 };
 type Mapgen = {
   overmap_terrains: OMT[];
+  rows: string[];
+  palettes: Map<string, Loot>[];
+  additional_items: Loot;
 };
+export function getAllMapgens(data: CddaData): Mapgen[] {
+  /*
+  data.byType<Mapgen>('mapgen');
+  mg.om_terrain
+  mapgen.object
+  mapgen.object.palettes
+  mapgen.object.rows
+  mapgen.object.place_item
+  mapgen.object.place_items
+  mapgen.object.place_loot
+  mapgen.object.add
+  */
+  throw "not implemented";
+}
+
 type LocationAndLoot = {
   mapgen: Mapgen;
-  loot: Map</*item_id*/ string, /*chance*/ number>;
+  loot: Loot;
 };
 export function getAllLocationsAndLoot(data: CddaData): LocationAndLoot[] {
   throw "not implemented";
@@ -20,7 +55,7 @@ type SpawnLocation = {
   singularName: string;
   // sym: string
   // color: string
-  chance: number;
+  chance: chance;
 };
 export function getItemSpawnLocations(
   data: CddaData,
