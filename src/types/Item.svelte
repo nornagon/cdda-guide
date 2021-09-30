bb85cd1a7d24e78123648f576d949136266263b0
Jeremy Rose <nornagon@nornagon.net>
2021-09-30 14:18:36 -0700

    show spoil multiplier on item pockets

--- src/types/Item.svelte
+++ src/types/Item.svelte
@@ -328,6 +328,10 @@ const ascii_picture =
         {/if}
         <dt>Moves to Remove Item</dt>
         <dd>{pocket.moves}</dd>
+        {#if (pocket.sealed_data?.spoil_multiplier ?? 1) !== 1.0}
+          <dt>Spoil Multiplier</dt>
+          <dd>{pocket.sealed_data.spoil_multiplier}</dd>
+        {/if}
         {#if pocket.flag_restriction}
           <dt>Flag Restriction</dt>
           <dd>
