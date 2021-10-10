ed1e767214259fb3ba0fbe284a158c5bb8327dee
Jeremy Rose <nornagon@nornagon.net>
2021-10-10 15:59:37 -0700

    simplify OvermapAppearance a little

--- src/types/item/OvermapAppearance.svelte
+++ src/types/item/OvermapAppearance.svelte
@@ -9,38 +9,34 @@ export let overmapSpecial: OvermapSpecial;
 
 const overmaps = [...(overmapSpecial.overmaps ?? [])];
 let minX = Infinity,
-  minY = Infinity,
-  minZ = Infinity;
+  minY = Infinity;
 let maxX = -Infinity,
-  maxY = -Infinity,
-  maxZ = -Infinity;
+  maxY = -Infinity;
 const overmapsByPoint = new Map<string, typeof overmaps[0]>();
 for (const om of overmaps) {
   const [x, y, z] = om.point;
+  if (z !== 0) continue;
   if (x < minX) minX = x;
   if (y < minY) minY = y;
-  if (z < minZ) minZ = z;
   if (x > maxX) maxX = x;
   if (y > maxY) maxY = y;
-  if (z > maxZ) maxZ = z;
-  overmapsByPoint.set(`${x}|${y}|${z}`, om);
+  overmapsByPoint.set(`${x}|${y}`, om);
 }
 const appearanceGrid: { sym: string; color: string; name: string }[][] = [];
-for (let z = 0; z < 1; z++)
-  for (let y = minY; y <= maxY; y++) {
-    const appearanceRow: { sym: string; color: string; name: string }[] = [];
-    for (let x = minX; x <= maxX; x++) {
-      const om = overmapsByPoint.get(`${x}|${y}|${z}`);
-      if (om) {
-        const omt_id = om.overmap.replace(/_(north|south|east|west)$/, "");
-        const appearance = omtAppearance(omt_id);
-        appearanceRow.push(appearance);
-      } else {
-        appearanceRow.push({ color: "black", sym: " ", name: "" });
-      }
+for (let y = minY; y <= maxY; y++) {
+  const appearanceRow: { sym: string; color: string; name: string }[] = [];
+  for (let x = minX; x <= maxX; x++) {
+    const om = overmapsByPoint.get(`${x}|${y}`);
+    if (om) {
+      const omt_id = om.overmap.replace(/_(north|south|east|west)$/, "");
+      const appearance = omtAppearance(omt_id);
+      appearanceRow.push(appearance);
+    } else {
+      appearanceRow.push({ color: "black", sym: " ", name: "" });
     }
-    appearanceGrid.push(appearanceRow);
   }
+  appearanceGrid.push(appearanceRow);
+}
 
 while (appearanceGrid.length) {
   const row = appearanceGrid[0];
