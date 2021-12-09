414cd7cc23481759a3bda4a524753c7bc3b28e7d
Jeremy Rose <nornagon@nornagon.net>
2021-12-09 15:58:10 +1100

    list items using an ammo type on ammunition_type page
    ref #43

--- src/types/AmmunitionType.svelte
+++ src/types/AmmunitionType.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 import { getContext } from "svelte";
-import { CddaData, singularName } from "../data";
-import type { AmmunitionType } from "../types";
+import { CddaData, mapType, singularName } from "../data";
+import LimitedList from "../LimitedList.svelte";
+import type { AmmunitionType, Item } from "../types";
+import ItemSymbol from "./item/ItemSymbol.svelte";
 import ThingLink from "./ThingLink.svelte";
 
 export let item: AmmunitionType;
@@ -14,6 +16,33 @@ const compatible = data.byType("item").flatMap((x) => {
   return [];
 });
 compatible.sort((a, b) => singularName(a).localeCompare(singularName(b)));
+
+const defaultPocketData = {
+  pocket_type: "CONTAINER",
+  min_item_volume: "0 ml",
+  moves: 100,
+  fire_protection: false,
+  watertight: false,
+  airtight: false,
+  open_container: false,
+  rigid: false,
+  holster: false,
+};
+
+const usesAmmoType = (w: Item, t: AmmunitionType): boolean => {
+  let pockets = (w.pocket_data ?? []).map((pocket) => {
+    return { ...defaultPocketData, ...pocket };
+  });
+  let ammo = pockets.flatMap((pocket) =>
+    pocket.pocket_type === "MAGAZINE"
+      ? Object.keys(pocket.ammo_restriction ?? {})
+      : []
+  );
+  return ammo.some((a) => a === t.id);
+};
+
+const usedBy = data.byType("item").filter((w) => usesAmmoType(w, item));
+usedBy.sort((a, b) => singularName(a).localeCompare(singularName(b)));
 </script>
 
 <h1>Ammunition Type: {singularName(item)}</h1>
@@ -27,4 +56,18 @@ compatible.sort((a, b) => singularName(a).localeCompare(singularName(b)));
       </li>
     {/each}
   </ul>
+</section>
+
+<section>
+  <h1>Used By</h1>
+  {#if usedBy.length > 0}
+    <LimitedList items={usedBy} let:item>
+      <ItemSymbol {item} />
+      <ThingLink type={mapType(item.type)} id={item.id} />
+    </LimitedList>
+  {:else}
+    <p>
+      <em style="color: var(--cata-color-gray)">No items use this ammo.</em>
+    </p>
+  {/if}
 </section>
