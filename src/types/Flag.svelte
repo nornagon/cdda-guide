3e7088ea55b9c76626757d3559c765245984fbb0
Jeremy Rose <nornagon@nornagon.net>
2022-01-30 14:38:18 -0800

    support <color_foo> in flag descriptions

--- src/types/Flag.svelte
+++ src/types/Flag.svelte
@@ -28,33 +28,45 @@ for (const it of data.byType("vehicle_part")) {
   }
 }
 
-let color = ["gray"];
-const spans: { string: string; color: string }[] = [];
-let remaining = item.info ?? "";
-while (true) {
-  const nextColorTag = remaining.match(/<\/?(info|good|bad)>/);
-  if (nextColorTag) {
-    if (nextColorTag.index > 0)
-      spans.push({
-        string: remaining.substr(0, nextColorTag.index),
-        color: color[0],
-      });
-    if (nextColorTag[0][1] === "/" && color.length > 1) {
-      color.shift();
-    } else {
-      color.unshift(nextColorTag[1]);
-    }
-    remaining = remaining.substr(nextColorTag.index + nextColorTag[0].length);
-  } else break;
+function parseColorText(text: string): { string: string; color: string }[] {
+  let color = ["gray"];
+  const spans: { string: string; color: string }[] = [];
+  let remaining = text;
+  while (true) {
+    const nextColorTag = remaining.match(
+      /<\/?(info|good|bad|color|color_[^>]+)>/
+    );
+    if (nextColorTag) {
+      if (nextColorTag.index > 0)
+        spans.push({
+          string: remaining.substring(0, nextColorTag.index),
+          color: color[0],
+        });
+      if (nextColorTag[0][1] === "/" && color.length > 1) {
+        color.shift();
+      } else {
+        color.unshift(nextColorTag[1]);
+      }
+      remaining = remaining.substring(
+        nextColorTag.index + nextColorTag[0].length
+      );
+    } else break;
+  }
+  if (remaining.length) {
+    spans.push({ string: remaining, color: color[0] });
+  }
+
+  return spans;
 }
-if (remaining.length) {
-  spans.push({ string: remaining, color: color[0] });
-}
+
+const spans = parseColorText(item.info ?? "");
 
 const colorLookup = (color: string): string => {
   if (color === "info") return "cyan";
   else if (color === "good") return "green";
   else if (color === "bad") return "red";
+  const m = /^color_(.+)$/.exec(color);
+  if (m) return m[1];
   return "gray";
 };
 </script>
