4ccb53e376097eadaac114456c557a18290997b7
Jeremy Rose <nornagon@nornagon.net>
2022-07-19 19:22:26 -0700

    parallelise tile image loading

--- src/tile-data.ts
+++ src/tile-data.ts
@@ -9,26 +9,28 @@ const fetchJson = async (url: string) => {
       `Error ${res.status} (${res.statusText}) fetching tile data`
     );
   const json = await res.json();
-  for (const chunk of json["tiles-new"]) {
-    const blob = await fetch(`${url}/${chunk.file}`).then((b) => b.blob());
-    const blobUrl = URL.createObjectURL(blob);
-    const img = new Image();
-    img.src = blobUrl;
-    try {
-      await new Promise((resolve, reject) => {
-        img.onload = resolve;
-        img.onerror = reject;
-      });
-    } finally {
-      URL.revokeObjectURL(blobUrl);
-    }
-    const nx =
-      (img.width / (chunk.sprite_width ?? json.tile_info[0].width)) | 0;
-    const ny =
-      (img.height / (chunk.sprite_height ?? json.tile_info[0].height)) | 0;
-    chunk.nx = nx;
-    chunk.ny = ny;
-  }
+  await Promise.all(
+    json["tiles-new"].map(async (chunk: any) => {
+      const blob = await fetch(`${url}/${chunk.file}`).then((b) => b.blob());
+      const blobUrl = URL.createObjectURL(blob);
+      const img = new Image();
+      img.src = blobUrl;
+      try {
+        await new Promise((resolve, reject) => {
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+      } finally {
+        URL.revokeObjectURL(blobUrl);
+      }
+      const nx =
+        (img.width / (chunk.sprite_width ?? json.tile_info[0].width)) | 0;
+      const ny =
+        (img.height / (chunk.sprite_height ?? json.tile_info[0].height)) | 0;
+      chunk.nx = nx;
+      chunk.ny = ny;
+    })
+  );
   return json;
 };
 
