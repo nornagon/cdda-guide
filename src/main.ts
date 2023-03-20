6b008da0e713c888c8bc25a264cb03b97a34a3ea
Jeremy Rose <nornagon@nornagon.net>
2023-03-20 13:55:07 -0700

    git sha is first 8, not last 8 🤦

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,13 @@ tx.init({
   token: "1/1d8c1f9e14b4c21d70dd3f6fccdd0ab16b691105",
 });
 
-function last(n: number, str: string): string {
-  return str.substring(str.length - n);
-}
-
 if (location.hostname !== "localhost")
   Sentry.init({
     dsn: "https://e7e132477a2844118b8f6d045a507e10@o318291.ingest.sentry.io/5665093",
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 0.2,
     ...(process.env.GITHUB_SHA && {
-      release: `cdda-guide@${last(8, process.env.GITHUB_SHA)}`,
+      release: `cdda-guide@${process.env.GITHUB_SHA.slice(0, 8)}`,
     }),
     denyUrls: [
       // Chrome extensions
