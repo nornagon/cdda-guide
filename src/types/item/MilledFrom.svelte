012f56bad1f9d47f3ebaa68dd4cc65874dd80831
Jeremy Rose <nornagon@nornagon.net>
2023-05-16 15:36:22 -0700

    whoops, forgot to add new file

---
+++ src/types/item/MilledFrom.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { getContext } from "svelte";
+import { byName, CddaData } from "../../data";
+import LimitedList from "../../LimitedList.svelte";
+import ThingLink from "../ThingLink.svelte";
+import type { Furniture, Terrain } from "../../types";
+import ItemSymbol from "./ItemSymbol.svelte";
+import { t } from "@transifex/native";
+
+export let item_id: string;
+
+const data = getContext<CddaData>("data");
+
+const milledFrom = data
+  .byType("item")
+  .filter((it) => it.id && it.milling?.into === item_id);
+
+milledFrom.sort(byName);
+</script>
+
+{#if milledFrom.length}
+  <section>
+    <h1>{t("Mill", { _context: "Obtaining" })}</h1>
+    <LimitedList items={milledFrom} let:item>
+      <ItemSymbol {item} />
+      <ThingLink type="item" id={item.id} />
+    </LimitedList>
+  </section>
+{/if}
