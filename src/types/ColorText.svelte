66bd5839f7899f37aca515fc87935c52aeb4d45a
Jeremy Rose <nornagon@nornagon.net>
2023-06-18 20:34:47 -0700

    support <neutral> in colored text

--- src/types/ColorText.svelte
+++ src/types/ColorText.svelte
@@ -8,7 +8,7 @@ function parseColorText(text: string): { string: string; color: string }[] {
   let remaining = text;
   while (true) {
     const nextColorTag = remaining.match(
-      /<\/?(info|good|bad|color|color_[^>]+)>/
+      /<\/?(info|good|bad|neutral|color|color_[^>]+)>/
     );
     if (nextColorTag && nextColorTag.index != null) {
       if (nextColorTag.index > 0)
@@ -35,10 +35,15 @@ function parseColorText(text: string): { string: string; color: string }[] {
 
 const spans = parseColorText(text ?? "");
 
+const colors: Record<string, string> = {
+  info: "cyan",
+  good: "green",
+  bad: "red",
+  neutral: "yellow",
+};
+
 const colorLookup = (color: string): string => {
-  if (color === "info") return "cyan";
-  else if (color === "good") return "green";
-  else if (color === "bad") return "red";
+  if (color in colors) return colors[color];
   const m = /^color_(.+)$/.exec(color);
   if (m) return m[1];
   return "gray";
