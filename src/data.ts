b0c2f8c99f0c7be7a0b96c762559708be4a136a1
Jeremy Rose <nornagon@nornagon.net>
2024-04-23 20:12:54 -0700

    lint

--- src/data.ts
+++ src/data.ts
@@ -1480,7 +1480,7 @@ class ReverseIndex<T extends keyof SupportedTypesWithMapped> {
     if (!this.#_index) {
       this.#_index = new Map();
       for (const item of this.data.byType(this.objType)) {
-        if (!('id' in item || 'result' in item)) continue;
+        if (!("id" in item || "result" in item)) continue;
         for (const id of this.fn(item)) {
           if (!this.#index.has(id)) this.#index.set(id, []);
           this.#index.get(id)!.push(item);
