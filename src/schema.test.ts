f867049fc64ec8e9b3ef789351b3be1af2acbf99
Jeremy Rose <nornagon@nornagon.net>
2024-07-29 14:36:35 +0100

    skip nursing_home_8

--- src/schema.test.ts
+++ src/schema.test.ts
@@ -71,7 +71,7 @@ const all = data._raw
   .map((x, i) => [x.type, id(x) ?? i, data._flatten(x)]);
 
 const skipped = new Set<string>([
-  "veggy_pickled_fried", // see https://github.com/CleverRaven/Cataclysm-DDA/pull/73406
+  "nursing_home_8", // See https://github.com/CleverRaven/Cataclysm-DDA/pull/75312
 ]);
 
 test.each(all)("schema matches %s %s", (type, id, obj) => {
