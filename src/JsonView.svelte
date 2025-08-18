25798d1fff79538a926741d6dcd9d77576d23c4c
esphas <esphas@hotmail.com>
2025-08-18 23:26:31 +0800

    mod support

--- src/JsonView.svelte
+++ src/JsonView.svelte
@@ -7,23 +7,58 @@ interface Props {
 
 let { obj, buildNumber }: Props = $props();
 const _context = "View/Edit on GitHub";
+
+const urlView = `https://github.com/CleverRaven/Cataclysm-DDA/blob/${
+  buildNumber ?? "master"
+}`;
+const urlEdit = `https://github.dev/CleverRaven/Cataclysm-DDA/blob/${
+  buildNumber ?? "master"
+}`;
+
+function allSources(o: any): any[] {
+  const sources: any[] = [];
+  sources.push(o.__self);
+  while (o.__prevSelf) {
+    sources.push(o.__prevSelf);
+    o = o.__prevSelf;
+  }
+  return sources.reverse();
+}
 </script>
 
 <pre>{JSON.stringify(
     obj,
-    (key, value) => (key === "__filename" ? undefined : value),
-    2,
+    (key, value) =>
+      ["__mod", "__filename", "__self", "__prevSelf"].includes(key)
+        ? undefined
+        : value,
+    2
   )}</pre>
-{#if obj.__filename}
-  <a
-    href={`https://github.com/CleverRaven/Cataclysm-DDA/blob/${
-      buildNumber ?? "master"
-    }/${obj.__filename}`}
-    target="_blank">{t("View", { _context })}</a>
+{#if obj.__prevSelf == null}
+  <a href={`${urlView}/${obj.__filename}`} target="_blank"
+    >{t("View", { _context })}</a>
   /
-  <a
-    href={`https://github.dev/CleverRaven/Cataclysm-DDA/blob/${
-      buildNumber ?? "master"
-    }/${obj.__filename}`}
-    target="_blank">{t("Edit on GitHub", { _context })}</a>
+  <a href={`${urlEdit}/${obj.__filename}`} target="_blank"
+    >{t("Edit on GitHub", { _context })}</a>
+{:else}
+  {#each allSources(obj) as o}
+    <details>
+      <summary>{o.__mod}</summary>
+      <pre>{JSON.stringify(
+          o,
+          (key, value) =>
+            ["__mod", "__filename", "__self", "__prevSelf"].includes(key)
+              ? undefined
+              : value,
+          2
+        )}</pre>
+      {#if o.__filename}
+        <a href={`${urlView}/${o.__filename}`} target="_blank"
+          >{t("View", { _context })}</a>
+        /
+        <a href={`${urlEdit}/${o.__filename}`} target="_blank"
+          >{t("Edit on GitHub", { _context })}</a>
+      {/if}
+    </details>
+  {/each}
 {/if}
